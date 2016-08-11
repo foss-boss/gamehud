050518c58dd2168b23e692e9422b4dba0ffc4520
Neil Ross <neil_2_15@hotmail.com>
2016-08-10 21:53:59 -0400

    neil change
    buh

--- Assignment 3/js/gameSample.js
+++ Assignment 3/js/gameSample.js
@@ -11,42 +11,35 @@ var bgImage = new Image();
 bgImage.onload = function () { 
  	bgReady = true; 
 }; 
-bgImage.src = "images/sampleBG.jpg"; 
+bgImage.src = "images/backgroundStage.jpg"; 
+ 
+//Health bar hearts
+var fullHeart = new Image();
+fullHeart.src = "images/heartFull.png"
+var halfHeart = new Image();
+halfHeart.src = "images/heartHalf.png"
+var emptyHeart = new Image();
+emptyHeart.src = "images/heartEmpty.png"
 
-//Weapon settings
-var pistol = new Image();
-pistol.src = "images/pistol.png"
-var pistolAmmo = 12;
-var machineGun = document.createElement("machineGun");
-var machineGunAmmo = 60;
-var weaponSelection = 1;
-var currentWeapon = "Pistol";
-var machineGunFiring;
-var minAmmo = 0;
-var knife = document.createElement("knife");
-
-//Gold settings
-var currentGold = 0; 
 
 // Health bar settings
-var lowHealthColor = "#FF0000";
-var lowHealthTreshold = 20;
+//var lowHealthColor = "#FF0000";
+//var lowHealthTreshold = 20;
 
-var mediumHealthColor = "#FFFF00";
-var mediumHealthTreshold = 60;
+//var mediumHealthColor = "#FFFF00";
+//var mediumHealthTreshold = 60;
 
-var highHealthColor = "#00FF00";
+//var highHealthColor = "#00FF00";
 
-var healthBarSlotHeight = 40;
-var healthBarSlotWidth = 20;
+//var healthBarSlotHeight = 40;
+//var healthBarSlotWidth = 20;
 
-var currentHP = 50;
+var currentHP = 6;
 var minHP = 0;
-var maxHP = 100;
-var HPIncreaseValue = 10;
-var HPBarOffset = 2;
-
-var increaseHP = function() {
+var maxHP = 6;
+var HPIncreaseValue = 1;
+//var HPBarOffset = 2;
+	var increaseHP = function() {
 	if (currentHP < maxHP)
 	{
 		currentHP += HPIncreaseValue;
@@ -59,32 +52,7 @@ var decreaseHP = function() {
 		currentHP -= HPIncreaseValue;
 	}
 }
-
-var fireWeapon = function(){
-	switch (weaponSelection){
-	case 1:
-		if (pistolAmmo != 0) {
-		pistolAmmo--;
-		}
-		break;
-	case 2: 
-	if (machineGunAmmo > 0) {
-		machineGunFiring = setInterval(function(){
-			machineGunAmmo--;}, 100);
-	}
-	else if(machineGunAmmo == 0) {
-		clearInterval(machineGunFiring);
-	}
-		break;
-	case 3:
-		break;
-	}
-}
-
-function stopMachineGun(){
-	clearInterval(machineGunFiring);
-}
-
+ 
 // Handle keyboard controls 
 addEventListener("keyup", function (e) { 
 	if (e.keyCode == 38) 
@@ -97,51 +65,8 @@ addEventListener("keyup", function (e) {
 		// player releasing down 
 		decreaseHP();
 	} 
-	if (e.keyCode == 49)
-	{
-		weaponSelection = 1;
-		currentWeapon = "Pistol";
-	}
-	if (e.keyCode == 50)
-	{
-		weaponSelection = 2;
-		currentWeapon = "Machine Gun";
-	}
-	if (e.keyCode == 51)
-	{
-		weaponSelection = 3;
-		currentWeapon = "Knife";
-	}
-	if (e.keyCode == 82)
-	{
-		switch (weaponSelection){
-			case 1:
-				setTimeout(function(){pistolAmmo = 12;}, 1500);
-				break;
-			case 2:
-				setTimeout(function(){machineGunAmmo = 60;}, 3000);
-				break;
-		}
-	}
-	if (e.keyCode == 71)
-	{
-		var howMuchGold = Math.random() * 100;
-		if (howMuchGold >= 0 && howMuchGold < 90) {
-			currentGold = currentGold + 1;
-		}
-		else if (howMuchGold >= 90 && howMuchGold < 99) {
-			currentGold = currentGold + 10;
-		}
-		else if (howMuchGold >= 99 && howMuchGold < 100) {
-			currentGold = currentGold + 100;
-		}
-	}
-}, false);
-
-//Handle mouse controls
-window.addEventListener("mousedown", fireWeapon, false);
-window.addEventListener("mouseup", stopMachineGun, false);
-
+}, false); 
+ 
 // Draw everything 
 var render = function () { 
 	if (bgReady) { 
@@ -153,29 +78,49 @@ var render = function () {
  	ctx.font = "24px Helvetica"; 
 	ctx.textAlign = "left"; 
 	ctx.textBaseline = "top"; 
-	ctx.fillText("Current HP: " + currentHP, 32, 32); 
+	//ctx.fillText("Current HP: " + currentHP, 850, 32); 
 	
-	//Display Current Gold
-	ctx.fillText("Gold: " + currentGold, 32, 100);
-	
-	//Ammo counter
-	switch (weaponSelection){
-		case 1:
-			ctx.fillText("Ammo: " + pistolAmmo, 955, 635);
-			break;
-		case 2:
-			ctx.fillText("Ammo: " + machineGunAmmo, 800, 720);
-			break;
-		case 3:
-			ctx.fillText("Ammo: --", 800, 720);
-			break;
+	//Health
+
+	if (currentHP == 6) {
+		ctx.drawImage(fullHeart, 800, 60, 70, 70);
+		ctx.drawImage(fullHeart, 870, 60, 70, 70);
+		ctx.drawImage(fullHeart, 940, 60, 70, 70);
+	}
+	else if (currentHP == 5) {
+		ctx.drawImage(fullHeart, 800, 60, 70, 70);
+		ctx.drawImage(fullHeart, 870, 60, 70, 70);
+		ctx.drawImage(halfHeart, 940, 60, 70, 70);
+	}
+	else if (currentHP == 4) {
+		ctx.drawImage(fullHeart, 800, 60, 70, 70);
+		ctx.drawImage(fullHeart, 870, 60, 70, 70);
+		ctx.drawImage(emptyHeart, 940, 60, 70, 70);
+	}
+	else if (currentHP == 3) {
+		ctx.drawImage(fullHeart, 800, 60, 70, 70);
+		ctx.drawImage(halfHeart, 870, 60, 70, 70);
+		ctx.drawImage(emptyHeart, 940, 60, 70, 70);
+	}
+	else if (currentHP == 2) {
+		ctx.drawImage(fullHeart, 800, 60, 70, 70);
+		ctx.drawImage(emptyHeart, 870, 60, 70, 70);
+		ctx.drawImage(emptyHeart, 940, 60, 70, 70);
+	}
+	else if (currentHP == 1) {
+				ctx.drawImage(halfHeart, 800, 60, 70, 70) ;
+				ctx.drawImage(emptyHeart, 870, 60, 70, 70);
+				ctx.drawImage(emptyHeart, 940, 60, 70, 70);
+	}
+	else if (currentHP == 0) {
+		ctx.drawImage(emptyHeart, 800, 60, 70, 70);
+		ctx.drawImage(emptyHeart, 870, 60, 70, 70);
+		ctx.drawImage(emptyHeart, 940, 60, 70, 70);
 	}
 	
-	//Display current weapon
-	//ctx.fillText("Weapon: " + currentWeapon, , 690);
-	
+
 	// Health Bar Rectangles
-	var currentColor = highHealthColor;
+	/*var currentColor = highHealthColor;
 	
 	if ( currentHP <= lowHealthTreshold )
 	{
@@ -189,18 +134,12 @@ var render = function () {
 	for (i = 0; i < currentHP/10; i++){
 		ctx.fillStyle = currentColor;
 		
-		var xPosition = 32 + (i*healthBarSlotWidth);
+		var xPosition = 850 + (i*healthBarSlotWidth);
 		
 		ctx.fillRect( xPosition, 60, healthBarSlotWidth, healthBarSlotHeight);
-	}
-	
-	//draw weapons
-	if (weaponSelection == 1) {
-		ctx.drawImage(pistol, 955, 660)
-	}
+	}*/
 }; 
 
- 
 // The main game loop 
 var main = function () { 
 
